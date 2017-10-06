66f166ab28cf27dac580cd5e94fa35116e3996a1
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2017-10-06 16:23:31 +0200

    Support subject alternative names.

--- test/units/certificateDetailsTests.js
+++ test/units/certificateDetailsTests.js
@@ -68,5 +68,36 @@ suite('certificateDetails', () => {
         done();
       });
     });
+
+    test('returns the certificate details with SANs.', done => {
+      certificateDetails.get(path.join(__dirname, '..', 'keys', 'localhost-with-san'), (err, details) => {
+        assert.that(err).is.null();
+        assert.that(details).is.ofType('object');
+        assert.that(details.subject).is.equalTo({
+          country: 'DE',
+          state: 'Baden-Wuerttemberg',
+          location: 'Riegel am Kaiserstuhl',
+          organisation: 'the native web GmbH',
+          commonName: 'localhost',
+          email: 'hello@thenativeweb.io',
+          alternativeNames: [ '1.2.3.4' ]
+        });
+        assert.that(details.issuer).is.equalTo({
+          country: 'DE',
+          state: 'Baden-Wuerttemberg',
+          location: 'Riegel am Kaiserstuhl',
+          organisation: 'the native web GmbH',
+          commonName: 'localhost',
+          email: 'hello@thenativeweb.io'
+        });
+        assert.that(details.metadata).is.equalTo({
+          validFrom: new Date(2017, 9, 6, 14, 15, 38),
+          validTo: new Date(2047, 9, 4, 14, 15, 38),
+          fingerprint: '7E:1B:07:5A:8A:F8:B2:96:0D:AA:0C:05:35:48:37:08:F8:34:B8:07',
+          serialNumber: 'DCF2E1CF89D0A5AD'
+        });
+        done();
+      });
+    });
   });
 });
